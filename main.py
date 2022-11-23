1ec506b7c5d521afaf459893ab0b5c0c7bbbfd53
Matheus Oliveira Silva <matheusolivsilv.job@gmail.com>
2022-11-23 14:32:38 -0300

    pdf e cdf adicionadas

--- main.py
+++ main.py
@@ -21,3 +21,31 @@ def desvio_padrao(rodada):
 
 # --------------------------------------------------------------------------------------------------------------------------------
 
+# Usaremos random para gerar os números pseudo-aleatórios
+import random as rd
+
+# --------------------------------------------------------------------------------------------------------------------------------
+
+# definindo número de euler para uso posterior:
+e = 2.71828182845904523536028747135266249775724709369995
+
+taxa_de_entrada = 1/10
+taxa_de_servico = 1/2
+
+# CDF do tempo entre chegadas:
+def A(t):
+    return 1 - e**(-(taxa_de_entrada*t))
+
+# pdf do tempo entre chegadas:
+def a(t):
+    return taxa_de_entrada*e**(-(taxa_de_entrada*t))
+
+
+# CDF do tempo de serviço:
+def B(t):
+    return 1 - e**(-(taxa_de_servico)*t)
+
+# pdf do tempo de serviço:
+def b(t):
+    return taxa_de_servico*e**(-(taxa_de_servico)*t)
+
